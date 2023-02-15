f09c4f135bfbc3b03b598c508c27e3f7837563ef
Gaasre <amine.barchid@gmail.com>
2023-02-15 15:02:28 +0100

    better SEO

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,14 @@
 	import Functionalities from '../components/functionalities.svelte';
 </script>
 
+<svelte:head>
+	<title>Footape - Streamline Your Video Production Process</title>
+	<meta
+		name="description"
+		content="Create and collaborate on YouTube videos with ease using Footape. Manage your video creation process, resources, progress, and costs all in one place."
+	/>
+</svelte:head>
+
 <Hero />
 <Services />
 <Functionalities />
