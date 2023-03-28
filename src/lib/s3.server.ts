4dff5ad08854c7ff0391b3abf73abaebe1a37dd4
Gaasre <amine.barchid@gmail.com>
2023-03-28 23:37:11 +0200

    fix errors

--- src/lib/s3.server.ts
+++ src/lib/s3.server.ts
@@ -1,7 +1,7 @@
 import S3 from 'aws-sdk/clients/s3'
 
 const s3Client = new S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY,
+    accessKeyId: process.env.AWS_PUBLIC_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     endpoint: process.env.AWS_S3_ENDPOINT,
     region: 'fr-par',
